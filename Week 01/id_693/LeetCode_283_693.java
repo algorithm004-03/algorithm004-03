4233d8e5c7b50a4fe134fa04b113b8fd0ef7a9ac
lilei <130808991@qq.com>
2019-10-14 21:20:31 +0800

    693-Week 01

--- Week 01/id_693/LeetCode_283_693.java
+++ Week 01/id_693/LeetCode_283_693.java
@@ -19,4 +19,19 @@ public class LeetCode_283_693 {
             }
         }
     }
+
+    public void moveZeroes_2(int[] nums) {
+        int len = nums.length;
+        for (int i = 0, j = 0; i < len; ++i) {
+            if (nums[i] != 0) {
+                nums[j] = nums[i];
+                if (j != i) {
+                    nums[i] = 0;
+                }
+                j++;
+            }
+        }
+    }
+
+
 }
