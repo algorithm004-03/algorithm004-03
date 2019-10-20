284f3377b00ee77ff686a837cafc0ab86abb5e51
lilei <130808991@qq.com>
2019-10-20 18:33:44 +0800

    693-Week 01

---
+++ Week 01/id_693/LeetCode_42_693.java
@@ -0,0 +1,29 @@
+package id_693;
+
+/**
+ * @Desc 42. 接雨水  https://leetcode-cn.com/problems/trapping-rain-water/
+ * @Auther 李雷(KyLin)
+ * @Date 2019/10/20
+ */
+public class LeetCode_42_693 {
+    //暴力破解
+    public int trap(int[] height) {
+        int max = 0;
+        for (int i = 0; i < height.length; i++) {
+            int leftMax = 0;
+            int rightMax = 0;
+            //以当前为起点，找出左边的最大值
+            for (int j = i; j >= 0; j--) {
+                leftMax = Math.max(leftMax,height[j]);
+            }
+            //以当前为起点找出右边的最大值
+            for (int j = i; j < height.length; j++) {
+                rightMax = Math.max(rightMax,height[j]);
+            }
+            //获得左右两边最大值中的最小值 减去自身即可（如果自身是最大值，那么就会是自身-自身）
+            max += Math.min(leftMax,rightMax) - height[i];
+        }
+
+        return max;
+    }
+}
