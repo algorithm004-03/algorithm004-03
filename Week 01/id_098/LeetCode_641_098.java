244225791ea70cc43c7cb5ff8d89f72d1c0cd8bd
yangyue <yangyue@carcredit.com.cn>
2019-10-20 17:49:45 +0800

    098-Week 01 LeetCode_641

--- Week 01/id_098/LeetCode_641_098.java
+++ Week 01/id_098/LeetCode_641_098.java
@@ -79,3 +79,73 @@ class MyCircularDeque {
  * boolean param_7 = obj.isEmpty();
  * boolean param_8 = obj.isFull();
  */
+
+//最优化 双端循环队列方案，设置另外变量count 数组是否满或者空，无空间浪费！！ （他人答案）
+class MyCircularDeque {
+
+    private int[] q;
+    private int head, tail, len;
+
+    public MyCircularDeque(int k) {
+        this.q = new int[k];
+    }
+
+    public boolean insertFront(int value) {
+        if (isFull()) return false;
+        if (!isEmpty()) head = (head - 1 + q.length) % q.length;
+        q[head] = value;
+        len++;
+        return true;
+    }
+
+    public boolean insertLast(int value) {
+        if (isFull()) return false;
+        if (!isEmpty()) tail = (tail + 1) % q.length;
+        q[tail] = value;
+        len++;
+        return true;
+    }
+
+    public boolean deleteFront() {
+        if (isEmpty()) return false;
+        if (len > 1) head = (head + 1) % q.length;
+        len--;
+        return true;
+    }
+
+    public boolean deleteLast() {
+        if (isEmpty()) return false;
+        if (len > 1) tail = (tail - 1 + q.length) % q.length;
+        len--;
+        return true;
+    }
+
+    public int getFront() {
+        return isEmpty() ? -1: q[head];
+    }
+
+    public int getRear() {
+        return isEmpty() ? -1: q[tail];
+    }
+
+    public boolean isEmpty() {
+        return len == 0;
+    }
+
+    public boolean isFull() {
+        return len == q.length;
+    }
+}
+
+/**
+ * Your MyCircularDeque object will be instantiated and called as such:
+ * MyCircularDeque obj = new MyCircularDeque(k);
+ * boolean param_1 = obj.insertFront(value);
+ * boolean param_2 = obj.insertLast(value);
+ * boolean param_3 = obj.deleteFront();
+ * boolean param_4 = obj.deleteLast();
+ * int param_5 = obj.getFront();
+ * int param_6 = obj.getRear();
+ * boolean param_7 = obj.isEmpty();
+ * boolean param_8 = obj.isFull();
+ */
