d4ed9e2632f6664cb4cdb2c8543e88d57765934b
whl <313576743@qq.com>
2019-10-23 17:37:42 +0800

    commit

--- Week 01/id_273/LeetCode_283_273.java
+++ Week 01/id_273/LeetCode_283_273.java
@@ -1,47 +1,45 @@
-//283 移动0
+//283. 移动0
 
-//解法1 双指针
-//通过k指针指向非零元素应该存放的位置
+//解法1：双指针
+//思路：定义一个慢指针K指向非零元素的index,遍历数组,若当前元素不为0,就在指针K位置赋值为当前元素,赋值后k++
+//		这样指针k的前半部分都是非零元素,后半部分补0即可
 //时间复杂度为O(n)
 //空间复杂度为O(1)
-//但是该方法有一个缺陷，那就是在i=k的时候进行了太多次无用的交换，解法2的代码对其优化
+//总结：学会活用指针
 public void moveZeroes(int[] nums) {
 	int k = 0;
-	for(int i=0;i<nums.length;i++) {
+	for (int i = 0; i<nums.length; i++) {
 		if (nums[i] != 0) {
-			int temp = nums[k];
-			nums[k++] = nums[i];
-			nums[i] = temp;
-		}
-	}
-}
-
-public void moveZeroes2(int[] nums) {
-	int k = 0;
-	for(int i = 0;i<nums.length;i++) {
-		if (nums[i] != 0) {
+			//优化不必要的赋值语句
 			if (i != k) {
 				nums[k] = nums[i];
-				nums[i] = 0;
 			}
 			k++;
 		}
+	}
+	for (int i = k; i<nums.length; i++) {
+		nums[i] = 0;
 	}
 }
 
-//解法2 也是双指针 只是先进行非零元素的赋值 在最后通过一次循环对零赋值
-//优化策略也是跟之前一样 避免i = k时不必要的赋值操作
-public static void moveZeroes3(int[] nums) {
+//解法2：辅助数组
+//思路：遍历数组,将非0元素存储到辅助数组中
+//时间复杂度O(n)
+//空间复杂度O(n)
+//总结：和解法1的思想类似,重要的是学会活用指针
+public void moveZeroes(int[] nums) {
 	int k = 0;
-	for (int i = 0;i<nums.length;i++) {
+	int[] temp = new int[nums.length];
+	for (int i = 0; i<nums.length; i++) {
 		if (nums[i] != 0) {
-			if(i != k) {
-				nums[k] = nums[i];
-			}
-			k++;
+			temp[k++] = nums[i];
 		}
 	}
-	for (int j = k;j<nums.length;j++) {
-		nums[j] = 0;
+	for (int i = 0; i<k; i++) {
+		nums[i] = temp[i];
+	}
+
+	for (int i = k; i<nums.length; i++) {
+		nums[i] = 0;
 	}
 }
