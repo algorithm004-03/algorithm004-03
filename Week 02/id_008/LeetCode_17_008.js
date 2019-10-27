579d3fbdeac35a8a14791d8207b74236ce389824
Fast Light <fastlight@fastlight.cn>
2019-10-27 23:58:59 +0800

    008-Week 02

---
+++ Week 02/id_008/LeetCode_17_008.js
@@ -0,0 +1,37 @@
+/**
+ * @param {string} digits
+ * @return {string[]}
+ */
+var letterCombinations = function(digits) {
+    var map = [[], [], ["a", "b", "c"], ["d", "e", "f"], ["g", "h", "i"], ["j", "k", "l"], ["m", "n", "o"], ["p", "q", "r", "s"], ["t", "u", "v"], ["w", "x", "y", "z"]];
+    var cache = {};
+    return gen(digits);
+
+    function gen(nums){
+        if (nums.length <= 0) {
+            return [];
+        }
+
+        if (typeof cache[nums] !== "undefined") {
+            return cache[nums];
+        }
+
+        var a = +nums.charAt(0);
+        var sub = gen(nums.substring(1));
+        var result = [];
+
+        if (nums.length === 1) {
+            cache[nums] = sub;
+            return sub;
+        }
+
+        for (var i = 0; i < map[a].length; ++i) {
+            for (var j = 0; j < sub.length; ++j) {
+                result.push(map[a][i] + sub[j]);
+            }
+        }
+
+        cache[nums] = result;
+        return result;
+    }
+};
---
+++ Week 02/id_008/LeetCode_297_008.js
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+
+/**
+ * Encodes a tree to a single string.
+ *
+ * @param {TreeNode} root
+ * @return {string}
+ */
+var serialize = function(root) {
+
+};
+
+/**
+ * Decodes your encoded data to tree.
+ *
+ * @param {string} data
+ * @return {TreeNode}
+ */
+var deserialize = function(data) {
+
+};
+
+/**
+ * Your functions will be called as such:
+ * deserialize(serialize(root));
+ */
