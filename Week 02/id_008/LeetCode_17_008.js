7bf2d9ea8fe01f74c0629794241599bd7c3fc525
Fast Light <fastlight@fastlight.cn>
2019-10-28 02:11:27 +0800

    008-Week 02

--- Week 02/id_008/LeetCode_17_008.js
+++ Week 02/id_008/LeetCode_17_008.js
@@ -5,25 +5,23 @@
 var letterCombinations = function(digits) {
     var map = [[], [], ["a", "b", "c"], ["d", "e", "f"], ["g", "h", "i"], ["j", "k", "l"], ["m", "n", "o"], ["p", "q", "r", "s"], ["t", "u", "v"], ["w", "x", "y", "z"]];
     var cache = {};
-    return gen(digits);
+    digits = digits.replace(/0/g, "").replace(/1/g, "");
+    return digits.length ? gen(digits) : [];
 
     function gen(nums){
-        if (nums.length <= 0) {
-            return [];
-        }
-
         if (typeof cache[nums] !== "undefined") {
             return cache[nums];
         }
 
         var a = +nums.charAt(0);
-        var sub = gen(nums.substring(1));
-        var result = [];
 
         if (nums.length === 1) {
             cache[nums] = map[a];
             return map[a];
         }
+
+        var sub = gen(nums.substring(1));
+        var result = [];
 
         for (var i = 0; i < map[a].length; ++i) {
             for (var j = 0; j < sub.length; ++j) {
