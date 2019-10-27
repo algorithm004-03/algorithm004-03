279694e7bbc4451aebc85affad5904f9fa1fa5eb
isaaczhou <isaaczhou85@gmail.com>
2019-10-26 22:32:25 -0400

    add LeetCode_169_118.py

--- Week 02/id_118/Python/LeetCode_169_118.py
+++ Week 02/id_118/Python/LeetCode_169_118.py
@@ -1,9 +1,11 @@
 from typing import List
 
 
-class Solution:
+class Solution1:
     """
     Solution 1 use hashmap
+    Time Complexity: O(n)
+    Space Complexity: O(n)
     """
 
     def majorityElement(self, nums: List[int]) -> int:
@@ -19,6 +21,24 @@ class Solution:
                 return num
 
 
+class Solution2:
+    """
+    Solution 2 use hashmap but with only 1 iteration
+    """
+
+    def majorityElement(self, nums: List[int]) -> int:
+        count = {}
+        for num in nums:
+            if num in count:
+                count[num] += 1
+                if count[num] > (len(nums) // 2):
+                    return num
+            else:
+                count[num] = 1
+
+
 if __name__ == '__main__':
-    sol = Solution()
-    print(sol.majorityElement([2, 2, 1, 1, 1, 2, 2]))
+    sol1 = Solution1()
+    sol2 = Solution2()
+    print(sol1.majorityElement([2, 2, 1, 1, 1, 2, 2]))
+    print(sol2.majorityElement([1, 1, 1, 2, 2]))
