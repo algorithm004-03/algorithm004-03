c49c88d3b83fb16c90a08b10d5ffb07b05632d01
zsc <35392577+zsc319@users.noreply.github.com>
2019-11-03 20:19:09 +0800

    search_in_rotated_sorted_array.cpp

---
+++ Week 03/id_673/search-in-rotated-sorted-array.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int lo = 0, hi = nums.size() - 1;
+        while (lo < hi) {
+            int mid = (lo + hi) / 2;
+            if ((nums[0] > target) ^ (nums[0] > nums[mid]) ^ (target > nums[mid]))
+                lo = mid + 1;
+            else
+                hi = mid;
+        }
+        return lo == hi && nums[lo] == target ? lo : -1;
+    }
+};
