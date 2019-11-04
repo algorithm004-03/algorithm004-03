cfebcdd99a1af5c0761567550e63ddea22b3c911
whl <313576743@qq.com>
2019-11-04 16:27:25 +0800

    commit

--- Week 02/id_273/LeetCode_242_273.java
+++ Week 02/id_273/LeetCode_242_273.java
@@ -1,39 +1,7 @@
 //242. 有效的字母异位词
 
-//解法1：首先是强行用HashMap做的解法，效率最低所以放第一个
-//思路：先将s存入HashMap，若出现重复value值+1，然后在map中查看是否包含t，找到时value-1，若value=1则移除key，若map最后为空说明有效。
-//时间复杂度O(n)，虽然和解法3时间复杂度相同，但是内部涉及到了大量的map操作
-public static boolean isAnagram1(String s, String t) {
-	if (s.length() != t.length()) return false;
-	Map<Character,Integer> map = new HashMap<>();
-	
-	for (char c : s.toCharArray()) {
-		Integer count = map.get(c);
-		//如果已经包含了该字符 value+1即可
-		if (map.containsKey(c)) {
-			map.put(c,++count);
-			continue;
-		}
-		if (count == null) {
-			map.put(c,1);
-		}
-	}
 
-	for (char c : t.toCharArray()) {
-		Integer count = map.get(c);
-		if (count == null) {
-			//说明不存在该字符
-			return false;
-		} else if (count > 1) {
-			map.put(c,--count);
-		} else {
-			map.remove(c);
-		}
-	}
-	return map.isEmpty();
-}
-
-//解法2：排序比较法
+//解法1：排序比较法		执行用时击败约51%
 //思路：将s和t转换为数组，然后比较排序后两个数组是否相同
 //时间复杂度O(nlogN)
 public static boolean isAnagram2(String s, String t) {
@@ -45,21 +13,22 @@ public static boolean isAnagram2(String s, String t) {
 	return Arrays.equals(temp1,temp2);
 }
 
-//解法3：数组映射法
+//解法2：数组映射法		执行用时击败约99%
 //思路：创建一个包含26位字符的数组，用于存储字符串中26个字符出现的次数
 //时间复杂度O(n)
-public static boolean isAnagram3(String s, String t) {
+//空间复杂度O(n)
+//总结：程序也可以不将字符串转换为数组直接判断, 但是效率并不高, 执行用时会多出大约4ms, 这里通过创建数组并直接对数组进行操作, 因此减少了一些运算量 
+public boolean isAnagram(String s, String t) {
+	char[] arr1 = s.toCharArray();
+	char[] arr2 = t.toCharArray();
 	if (s.length() != t.length()) return false;
-	int[] temp = new int[26];
-	for (int i = 0;i<s.length();i++) {
-		temp[s.charAt(i) - 'a']++;
-		temp[t.charAt(i) - 'a']--;
+	int[] temp = new int['z' + 1];
+	for (int i = 0; i < arr1.length(); i++) {
+		temp[arr1[i]]++;
+		temp[arr2[i]]--;
 	}
-	for (int i = 0;i<s.length();i++) {
-		if (temp[i] != 0) {
-			return false;
-		}
+	for (int a : temp) {
+		if (a != 0) return false;
 	}
 	return true;
 }
-
