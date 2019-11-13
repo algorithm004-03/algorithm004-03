52c72a2207b6c7e47d48789310ca940c1abebb29
lilei <130808991@qq.com>
2019-11-13 13:47:18 +0800

    修改错误题解

--- Week 05/id_693/practise/LeetCode_62_693.java
+++ Week 05/id_693/practise/LeetCode_62_693.java
@@ -71,7 +71,7 @@ public class LeetCode_62_693 {
         for (int i = 0; i < m; i++) temp[i][0] = 1;
         for (int i = 1; i < m; i++) {
             for (int j = 1; j < n; j++) {
-                temp[i][j] += temp[i - 1][j] + temp[i][j - 1];
+                temp[i][j] = temp[i - 1][j] + temp[i][j - 1];
             }
         }
         return temp[m - 1][n - 1];
