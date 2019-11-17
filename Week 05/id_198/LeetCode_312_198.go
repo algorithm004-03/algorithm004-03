13505b70b2edc1beaa74a6f951f85ddc6f010056
Reven <alloctag@github.com>
2019-11-17 23:47:58 +0800

    Week 05

--- Week 05/id_198/LeetCode_312_198.go
+++ Week 05/id_198/LeetCode_312_198.go
@@ -16,13 +16,13 @@ func maxCoins(nums []int) int {
 	v = append(v, 1)
 	n := len(nums)
 	dp := make([][]int, n+2)
-	for i := 0; i < n+1; i++ {
+	for i := 0; i < n+2; i++ {
 
 		dp[i] = make([]int, n+2)
 	}
-	for c := 1; c <= n; c++ {
-		for i := 1; i+c-1 <= n; i++ {
-			j := i + c - 1
+	for s := 1; s <= n; s++ {
+		for i := 1; i+s-1 <= n; i++ {
+			j := i + s - 1
 			for k := i; k <= j; k++ {
 
 				dp[i][j] = max(dp[i][j], v[i-1]*v[k]*v[j+1]+dp[i][k-1]+dp[k+1][j])
