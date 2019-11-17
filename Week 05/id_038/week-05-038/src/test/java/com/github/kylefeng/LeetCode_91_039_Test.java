bdc14206def67ec9ffe302c76c9e91d7dbc3099f
kylefeng <kai.feng02@enmotech.com>
2019-11-17 23:35:25 +0800

    Add week 05

---
+++ Week 05/id_038/week-05-038/src/test/java/com/github/kylefeng/LeetCode_91_039_Test.java
@@ -0,0 +1,19 @@
+package com.github.kylefeng;
+
+import org.junit.jupiter.api.Test;
+
+import static com.github.kylefeng.LeetCode_91_038.solution;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * @author kylefeng
+ * @time 2019/11/13 17:39
+ */
+public class LeetCode_91_039_Test {
+    @Test
+    void testSolution() {
+        assertEquals(2, solution("12"));
+        assertEquals(3, solution("226"));
+    }
+
+}
