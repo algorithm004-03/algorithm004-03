29b376bdc66247f786561122e51eb655da2f35dd
whl <313576743@qq.com>
2019-11-29 18:09:05 +0800

    commit

--- Week 02/id_273/LeetCode_242_273.java
+++ Week 02/id_273/LeetCode_242_273.java
@@ -1,7 +1,7 @@
 //242. 有效的字母异位词
 
 
-//解法1：排序比较法		执行用时击败约51%
+//解法1：排序比较法		执行用时：5ms
 //思路：将s和t转换为数组，然后比较排序后两个数组是否相同
 //时间复杂度O(nlogN)
 public static boolean isAnagram2(String s, String t) {
@@ -13,22 +13,21 @@ public static boolean isAnagram2(String s, String t) {
 	return Arrays.equals(temp1,temp2);
 }
 
-//解法2：数组映射法		执行用时击败约99%
+//解法2：数组映射法		执行用时：2ms
 //思路：创建一个包含26位字符的数组，用于存储字符串中26个字符出现的次数
 //时间复杂度O(n)
 //空间复杂度O(n)
-//总结：程序也可以不将字符串转换为数组直接判断, 但是效率并不高, 执行用时会多出大约4ms, 这里通过创建数组并直接对数组进行操作, 因此减少了一些运算量 
-public boolean isAnagram(String s, String t) {
-	char[] arr1 = s.toCharArray();
-	char[] arr2 = t.toCharArray();
+ public boolean isAnagram(String s, String t) {
 	if (s.length() != t.length()) return false;
-	int[] temp = new int['z' + 1];
-	for (int i = 0; i < arr1.length(); i++) {
-		temp[arr1[i]]++;
-		temp[arr2[i]]--;
+	char[] sc = s.toCharArray();
+	char[] tc = t.toCharArray();
+	int[] temp = new int[26];
+	for (int i = 0; i < sc.length; i++) {
+		temp[sc[i] - 'a']++;
+		temp[tc[i] - 'a']--;
 	}
-	for (int a : temp) {
-		if (a != 0) return false;
+	for (int i = 0; i < temp.length; i++) {
+		if (temp[i] != 0) return false; 
 	}
 	return true;
 }
