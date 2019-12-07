545a40b9af29dc313886e98aa8ccc844ff388293
ly刘莹(IT) <liuyinga@ctrip.com>
2019-12-07 15:44:03 +0800

    283-Week 08

---
+++ Week 08/id_283/LeetCode_5_283.java
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode id=5 lang=java
+ *
+ * [5] Longest Palindromic Substring
+ */
+
+// @lc code=start
+class Solution {
+    public String longestPalindrome(String s) {
+        if (s == null || s.length() < 1) 
+        {
+            return "";
+        }
+        int start = 0, end = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int len = Math.max(expand(s, i, i), expand(s, i, i + 1));
+            if (len > end - start) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+        }
+        return s.substring(start, end + 1);
+    }
+
+    
+    private int expand(String s, int left, int right) {
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
+        }
+        return right - left - 1;
+    }
+}
+// @lc code=end
+
