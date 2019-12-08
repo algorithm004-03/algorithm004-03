f9a213c3c3a0ab4d372b152a269864a0f3617599
kylefeng <kai.feng02@enmotech.com>
2019-12-08 22:50:43 +0800

    init

---
+++ Week 08/id_038/week-08-038/src/main/java/com/github/kylefeng/LeetCode_300_038.java
@@ -0,0 +1,25 @@
+package com.github.kylefeng;
+
+/**
+ * @author kylefeng
+ * @time 2019/12/8 16:03
+ */
+public class LeetCode_300_038 {
+    public static int lengthOfLIS(int[] nums) {
+        int[] tails = new int[nums.length];
+        int size = 0;
+        for (int x : nums) {
+            int i = 0, j = size;
+            while (i != j) {
+                int m = (i + j) / 2;
+                if (tails[m] < x)
+                    i = m + 1;
+                else
+                    j = m;
+            }
+            tails[i] = x;
+            if (i == size) ++size;
+        }
+        return size;
+    }
+}
