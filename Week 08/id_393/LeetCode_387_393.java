227d9eaf07596081e635f7e368da89873d858e62
boyiren <47518785+boyiren@users.noreply.github.com>
2019-12-08 22:52:31 +0800

    393-Week 08

---
+++ Week 08/id_393/LeetCode_387_393.java
@@ -0,0 +1,27 @@
+package no387;
+
+import java.util.HashMap;
+
+/**
+ * @author boyiren
+ * @date 2019-12-08
+ */
+public class Solution {
+    public int firstUniqChar(String s) {
+        HashMap<Character, Integer> count = new HashMap<Character, Integer>();
+        int n = s.length();
+        // build hash map : character and how often it appears
+        for (int i = 0; i < n; i++) {
+            char c = s.charAt(i);
+            count.put(c, count.getOrDefault(c, 0) + 1);
+        }
+
+        // find the index
+        for (int i = 0; i < n; i++) {
+            if (count.get(s.charAt(i)) == 1) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
